9aaad2209af3e95e70f7b396aa2e46950f90c53b
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-02-23 01:05:13 +0530

    major: tailwindcss integration success v1.0.4

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-const styles = {
-    loader: {
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "black",
-    },
-    loading: {
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        border: "4px solid white",
-    },
-    logo: {
-        border: "4px solid white",
-        width: "150px",
-        height: "150px",
-        borderRadius: "50%",
-        marginRight: "1rem",
-        marginBottom: "2rem",
-        animation: "spin 2s linear infinite",
-    },
-    text: {
-        color: "white",
-        fontFamily: "Arial, sans-serif",
-        fontSize: "1.5rem",
-    },
-}
+import React from "../node_modules/react";
+import ReactDOM from "../node_modules/react-dom/client";
+import logo from "../../../PetCare/client/src/assets/images/avatar.png";
+import "./index.css";
+
 
 function Loader({ logo }) {
   return (
@@ -45,7 +17,10 @@ function Loader({ logo }) {
           className="border-4  border-white w-[150px] sm:w-[200px] rounded-[50%] sm:mr-[1rem] sm:mb-[0] mb-[2rem] animate-spin"
         />
         {/* Loading text */}
-        <h5 className="text-white font-primary text-[1rem] vvsm:text-[1.5rem] vsm:text-[1.7rem] md:text-[2.5rem] sm:text-[2rem] ">
+        <h5
+          style={{ fontFamily: "var(--fontPrimary)" }}
+          className="textt text-white  text-[1rem] vvsm:text-[1.5rem] vsm:text-[1.7rem] md:text-[2.5rem] sm:text-[2rem] "
+        >
           Loading...
         </h5>
       </div>
@@ -53,4 +28,9 @@ function Loader({ logo }) {
   );
 }
 
-export default Loader;
+// export default Loader;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Loader logo={logo} />);
+
+// ReactDOM.render(<Loader />);
